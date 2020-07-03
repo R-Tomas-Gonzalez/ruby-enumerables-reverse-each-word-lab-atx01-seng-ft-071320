3aea7cfe385924cea1b1d444527329583fba01c3
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-03 23:13:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
-  reversed_word_string = ""
+  new_sentence = sentence.split
+  reversed_word_string = []
   new_sentence.each do |word|
     reversed_word == word.reverse
     reversed_word_string << reversed_word
