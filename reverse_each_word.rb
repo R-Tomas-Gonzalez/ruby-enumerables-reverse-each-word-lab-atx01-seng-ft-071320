925dd5390cd6cc1b73b044fae743d4faa752536e
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-03 22:49:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
-  new_sentence = sentence.reverse_each   
+  new_sentence = sentence.split
+  new_sentence.each do |word|
+    word.reverse_each
   end
 end
