cf42cf05c881ab74f4a3d5ffd5bf4d8e3309507e
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-03 23:31:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split
   new_sentence = []
   word_array.each do |word|
-    rev_word = word.reverse
-    new_sentence << rev_word
+    new_sentence << word.reverse
   end
   new_sentence.join(" ")
 end
